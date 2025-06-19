d7fdfae2dffdb8c7e80eda02559ff4f20e0d7043
Pierre GAUTHE <pierre.gauthe@allianz.fr>
2025-06-19 15:39:34 +0200

    Catch automatique, recup de sources

--- pgm_extractions/catch.bat
+++ pgm_extractions/catch.bat
@@ -1,17 +1,27 @@
 @echo off
+setlocal enabledelayedexpansion
 
 REM Variables
 set SERVER=sasprod
 set USER=gauthpi
 set REMOTE_PATH=/sasprod/produits/SASEnterpriseBIServer/segrac/METGTCCL/Pierre_G/temp/
 set LOCAL_PATH=C:\Users\GAUTHPI\Documents\PSAP\PSAP_VIZ\version_juin_2025\Nouveau_dossier\SUIVI_DC\data
-set FILES=psp_t_dc_190625.csv psp_ref_deleg_190625.csv
 set PSCP_PATH=C:\Users\GAUTHPI\Documents\PSAP\PSAP_VIZ\version_juin_2025\Nouveau_dossier\SUIVI_DC\ressources\pscp.exe
+set PLINK_PATH=C:\Users\GAUTHPI\Documents\PSAP\PSAP_VIZ\version_juin_2025\Nouveau_dossier\SUIVI_DC\ressources\plink.exe
+
+REM Initialize FILES variable
+set FILES=
+
+REM Use PLINK to list files on the remote server and filter them
+for /f "tokens=*" %%F in ('%PLINK_PATH% -ssh %USER%@%SERVER% "ls %REMOTE_PATH%psp*.csv"') do (
+    set FILES=!FILES! %%F
+)
 
 REM Loop through the files and download each one
-for %%F in (%FILES%) do (
-    %PSCP_PATH% %USER%@%SERVER%:%REMOTE_PATH%%%F %LOCAL_PATH%
-    if %ERRORLEVEL%==0 (
+for %%F in (!FILES!) do (
+    echo Downloading %%F...
+    %PSCP_PATH% %USER%@%SERVER%:"%%~F" "%LOCAL_PATH%"
+    if !ERRORLEVEL!==0 (
         echo Downloaded %%F successfully.
     ) else (
         echo Failed to download %%F.
---
+++ pgm_extractions/t_contrat.sas
@@ -0,0 +1,17 @@
+/* Recup des sources pour outil viz DC */
+%INCLUDE "/sasprod/produits/SASEnterpriseBIServer/segrac/METGTCCL/Pierre_G/Password/Connection_auto_encod.sas";
+libname dtm "/sasprod/produits/SASEnterpriseBIServer/segrac/METGTCCL/_sasdev/DATAMART/output/last";
+libname out "/sasprod/produits/SASEnterpriseBIServer/segrac/METGTCCL/Pierre_G/temp";
+
+/* recup_date */
+data _null_;
+    current_date = date();
+    call symputx('current_date', put(current_date, ddmmyyn6.));
+run;
+
+/* t_cnt */
+proc export data=dtm.t_contrat
+    outfile="/sasprod/produits/SASEnterpriseBIServer/segrac/METGTCCL/Pierre_G/temp/psp_t_cnt_&current_date..csv"
+    dbms=csv
+    replace;
+run;
